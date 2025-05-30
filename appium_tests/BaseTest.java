8a746352e9d7034263e263f945cd37a7d8d18351
DevBhagat2004 <163807837+DevBhagat2004@users.noreply.github.com>
2025-05-30 17:49:35 -0500

    Update BaseTest.java

--- appium_tests/BaseTest.java
+++ appium_tests/BaseTest.java
@@ -1,1 +1,40 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+    protected static AppiumDriver driver;
+
+    public static void setup() {
+        try {
+            // Basic setup for Android
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("platformName", "Android");
+            caps.setCapability("deviceName", "Android Emulator");
+            caps.setCapability("automationName", "UiAutomator2");
+
+            //  Replace with your app's package and activity
+            caps.setCapability("appPackage", "com.example.app"); 
+            caps.setCapability("appActivity", "com.example.app.MainActivity"); 
+
+            caps.setCapability("noReset", true);
+
+            // Connect to Appium server
+            driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void tearDown() {
+        // Close the session
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
 
